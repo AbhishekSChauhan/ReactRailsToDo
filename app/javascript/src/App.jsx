5f97e9674040ac81144b3dbd61b23b7ae55e785d
Abhishek Singh <abhishek.uietpuchd@gmail.com>
2021-07-13 18:11:49 +0530

    Implemented create action

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "components/Dashboard";
+import CreateTask from "components/Tasks/CreateTask";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -13,7 +14,8 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/dashboard" component={props=> <Dashboard {...props}/>} />
+      <Route exact path="/tasks/create" component={CreateTask} />
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
